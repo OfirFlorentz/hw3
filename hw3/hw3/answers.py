0f52fd21a2022cfb3bd842bb82699ae4a55b59bb
amitzri3 <amitzri3@gmail.com>
2022-01-10 23:32:12 +0200

    part 2 answers 1,2,3

--- hw3/hw3/answers.py
+++ hw3/hw3/answers.py
@@ -75,6 +75,15 @@ def part2_dropout_hp():
 part2_q1 = r"""
 **Your answer:**
 
+We can see that without dropout, we got nearly 100% accuracy on the training set, which implies on overfitting,
+while with dropout we got less accuracy on the training set and higher accuracy on the test set.
+Which is in correlation to our expectation because we learned that adding dropout is affected by reducing the overfitting.
+
+Also we can see difference between the 0.4 dropout and the 0.8 dropout whereas on the 0.8 we got less accuracy on the
+training compared to the 0.4 dropout set which implies on reducing the overfitting, but on the 0.4 dropout we got
+much higher accuracy on the test set, which might be because on the 0.8 we lose too much data and therefore reducing
+the tests set accuracy.
+
 
 Write your answer using **markdown** and $\LaTeX$:
 ```python
@@ -87,6 +96,11 @@ An equation: $e^{i\pi} -1 = 0$
 
 part2_q2 = r"""
 **Your answer:**
+
+Yes. As we can see from the graphs, from epochs 25-30 on the test set, we get increasing loss in the 0.4 dropout while
+on the same epoch we get increasing accuracy.
+That can explained by the fact that in order for the accuracy to change it needs to cross certain threshold, while the
+loss can also be affected by minor changes, causing the effect we just described. 
 
 
 Write your answer using **markdown** and $\LaTeX$:
@@ -101,6 +115,21 @@ An equation: $e^{i\pi} -1 = 0$
 part2_q3 = r"""
 **Your answer:**
 
+1.
+    Back propagation is a way of calculating gradients for the weights of the network,
+    whereas Gradient descent is algorithm for reaching local minima of the loss function.
+2.
+    In Gradient-Descent we use the entire training set (Epoch) to calculate the gradient and then adjust the weights
+     accordingly.
+    In Stochastic gradient descent we adjust the weights after every smaller amount of samples,
+     depend on the stochastic algorithm, and calculate the gradient according to those samples.
+3.
+    A. Normal Gradient descent is more likely to stuck on a local minima, whereas SGD is more likely  to avoid local minima
+        resulting in better results.
+    B. Normal Gradient descent is slower because we first need to predict the entire training set and then calculate
+        the gradient according to that. In SGD the calculation time is much faster because we calculate the gradient on
+         smaller amount of samples. 
+    
 
 Write your answer using **markdown** and $\LaTeX$:
 ```python
